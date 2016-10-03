1b83773622e7d403847b0c0db50dd32501fcd817
pinemood <kato@schwarzdesigns.de>
2016-10-03 23:19:53 +0200

    tests

---
+++ history.R
@@ -0,0 +1,97 @@
+install.packages("twitteR")
+install.packages("streamR", "RCurl", "ROAuth", "RJSONIO")
+library(devtools)
+install.packages("streamR")
+library(devtools)
+install.packages("devtools")
+library(devtools)
+library("devtools")
+install_github("streamR", "pablobarbera", subdir = "streamR")
+library(ROAuth)
+library(twitteR)
+library("twitteR")
+install.packages("twitteR")
+library(twitteR)
+library("twitteR")
+install.packages("twitteR")
+library("twitteR")
+install.packages("twitteR")
+library("twitteR")
+install.packages("twitteR")
+library("twitteR")
+install.packages("devtools", lib="~/R/lib")
+install.packages("twitteR")
+library("twitteR")
+library("bit", lib.loc="~/R/i686-pc-linux-gnu-library/3.2")
+install.packages("twitteR")
+library("bit", lib.loc="~/R/i686-pc-linux-gnu-library/3.2")
+library("bit", lib.loc="~/R/lib")
+install.packages("twitteR")
+install.packages("twitteR")
+install.packages("twitteR")
+install.packages("devtools", lib="~/R/lib")
+install.packages("twitteR")
+library(twitteR)
+requestURL <-  "https://api.twitter.com/oauth/dXYon3z42FcfFB1aTW4sqihp8"
+accessURL <-  " https://api.twitter.com/oauth/dXYon3z42FcfFB1aTW4sqihp8"
+authURL      <-  "https://api.twitter.com/oauth/dXYon3z42FcfFB1aTW4sqihp8"
+rm(requestURL)
+rm(requesURL)
+rm(accessURL)
+rm(authURL)
+authURL      <-  "https://api.twitter.com/oauth/authorize"
+accessURL <-  "https://api.twitter.com/oauth/access_token"
+requestURL <-  "https://api.twitter.com/oauth/request_token"
+consumerKey <-  "dXYon3z42FcfFB1aTW4sqihp8"
+consumerSecret <-  "06BXdz4FSWJrTYSGLkHgjnwO4caysaIK1n4RbG1GhHHy39VkZ4"
+Cred <- OAuthFactory$new(consumerKey=consumerKey,
+                         consumerSecret=consumerSecret,
+                         requestURL=requestURL,
+                         accessURL=accessURL,
+                         authURL=authURL)
+getTwitterOAuth(dXYon3z42FcfFB1aTW4sqihp8, 06BXdz4FSWJrTYSGLkHgjnwO4caysaIK1n4RbG1GhHHy39VkZ4)
+getTwitterOAuth(dXYon3z42FcfFB1aTW4sqihp8, 06BXdz4FSWJrTYSGLkHgjnwO4caysaIK1n4RbG1GhHHy39VkZ4)
+consumerSecret <-  "06BXdz4FSWJrTYSGLkHgjnwO4caysaIK1n4RbG1GhHHy39VkZ4"
+getTwitterOAuth(consumer_key, consumer_secret)
+library(ROAuth)
+install.packages("ROAuth")
+library(ROAuth)
+Cred <- OAuthFactory$new(consumerKey=consumerKey,
+                         consumerSecret=consumerSecret,
+                         requestURL=requestURL,
+                         accessURL=accessURL,
+                         authURL=authURL)
+download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
+Cred$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+registerTwitterOAuth(Cred)
+registerTwitterOAuth(Cred)
+library(ROAuth)
+registerTwitterOAuth(Cred)
+setup_twitter_oauth(CUSTOMER_KEY, CUSTOMER_SECRET, ACCESS_TOKEN, ACCESS_secret, credentials_file=NULL)
+setup_twitter_oauth(CUSTOMER_KEY, CUSTOMER_SECRET, ACCESS_TOKEN, ACCESS_secret)
+api_key = "dXYon3z42FcfFB1aTW4sqihp8"
+api_secret = "06BXdz4FSWJrTYSGLkHgjnwO4caysaIK1n4RbG1GhHHy39VkZ4"
+access_token = "744292076418916356-hKpio5Ji6NZlDqPFX9HdSKPaiPYO4SI"
+access_token_secret = "qanXLljnEPvHuxeTBce3bt54is7G8iG7FJhkNwruMsdlz"
+setup_twitter_oauth(api_key,api_secret,access_token,
+                    access_token_secret)
+registerTwitterOAuth(Cred)
+save(list="Cred", file="twitteR_credentials")
+library(twitteR)
+load("twitteR_credentials")
+registerTwitterOAuth(Cred)
+setup_twitter_oauth(CUSTOMER_KEY, CUSTOMER_SECRET, ACCESS_TOKEN, ACCESS_secret)
+registerTwitterOAuth(Cred)
+registerTwitterOAuth(Cred)
+install.packages(c("devtools", "rjson", "bit64", "httr"))
+install.packages(c("devtools", "rjson", "bit64", "httr"))
+setup_twitter_oauth(Cred)
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+searchTwitter("iphone")
+library(streamR)
+install.packages("streamR")
+install.packages("ROAuth")
+install.packages("ROAuth")
+install.packages("ROAuth")
+setup_twitter_oauth(Cred)
+setup_twitter_oauth(Cred)
